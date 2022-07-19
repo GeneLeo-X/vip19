318fa8b64906ad646a40000ad82f6e8e87e7ca57
车成业 <262926400@qq.com>
2022-07-19 16:48:38 +0800

    set集合

--- src/com/lovecoding/day10/Example02.java
+++ src/com/lovecoding/day10/Example02.java
@@ -1,7 +1,6 @@
 package com.lovecoding.day10;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Collection
@@ -13,7 +12,17 @@ import java.util.List;
  *           数组长度会自动扩容到（原数组的长度 + 原数组长度 >> 1)
  *           通常情况下为了防止过多次数组扩容，可以进行初始化容量配置。
  *           线程不安全的，适合单线程下使用。
+ *           适合添加与更新操作，查询
+ *      - Vector ：
+ *            适合添加与更新操作，查询
+ *            多线程下建议使用
+ *      - LinkedList :
+ *          插入与删除操作
+ *          方法非线程安全的，建议单线程下使用。
  *  - Set
+ *     - HashSet :
+ *          数据结构 哈希表 （HashMap） ，不允许存储重复元素的，单元素可以为null
+ *          。与存储顺序无关的。
  */
 public class Example02 {
 
@@ -72,5 +81,163 @@ public class Example02 {
         System.out.println(list1);
         //list1.s
 
+        System.out.println("------------向量应用-----------");
+
+        vector();
+
+        System.out.println("----------LinkedList应用----------");
+
+        linkedList();
+
+        System.out.println("-------------hashSet------------");
+
+        hashSet();
+    }
+
+
+    /**
+     * Vector : 动态数组
+     *   1、初始化时数组容量大小10 ， 增量0
+     *   2、扩容，若增量大于0 ，扩容到 原数组长度 + 增量大小。否则 扩容到 ，原数组长度 * 2
+     *   3、线程安全的，多线程下建议使用。
+     *
+     */
+    private static void vector(){
+
+        List<Integer> vector = new Vector<>(20 , 5);
+
+        vector.add(11);
+        vector.add(22);
+        vector.add(5);
+        vector.add(33);
+
+        System.out.println(vector.size());
+
+        System.out.println(vector);
+    }
+
+    /**
+     * 数据结构：双端链表
+     *  适合插入与删除操作
+     */
+    private static void linkedList(){
+
+        LinkedList<String> list = new LinkedList<>();
+        list.add("tom");
+        list.add("lily");
+
+        for(String str : list) {
+            System.out.println(str);
+        }
+
+        System.out.println("-----------------");
+
+        String first = list.getLast();
+
+        System.out.println(first);
+
+        System.out.println(list.poll());//获取并移除第一个元素
+
+        System.out.println("--------poll--------");
+        for(String str : list) {
+            System.out.println(str);
+        }
+
+    }
+
+    /**
+     * HashSet
+     *  - 不保证顺序恒久不变， 涉及到扩容的时候，会从新排列哈希表。
+     *  - 底层使用HashMap结构。map的key值使用的是元素值，val使用的 new Object() 进行占位.
+     *  - 根据HashCode值进行重复判定的。若想要达到重复的项不添加进去的话，必须保证hashCode值一致
+     *  - 判定元素值：排除重复元素，是先判断hashCode是否相同，再判断equals是否相等
+     */
+    private static void hashSet(){
+
+        HashSet<String> set = new HashSet();
+
+        set.add("tom");
+        System.out.println("tom".hashCode());
+        set.add("lucy");
+        set.add("lily");
+        System.out.println("abe".hashCode());
+        set.add("abe");
+        System.out.println("tom".hashCode());
+        set.add("tom");
+
+        for(String s : set) {
+            System.out.println(s);
+        }
+
+        System.out.println("--------------------");
+        set.remove("tom");
+
+        for(String s : set) {
+            System.out.println(s);
+        }
+
+        System.out.println("--------set中存储自定义对象----------");
+
+        HashSet<Student> hs = new HashSet<>();
+
+        Student s1 = new Student("小王", 11);
+        hs.add(s1);
+
+        System.out.println("s1 = " +s1.hashCode());
+
+        Student s2 = new Student("小张", 22);
+        hs.add(s2);
+
+        System.out.println("s2 = " + s2.hashCode());
+
+        Student s3 = new Student("小王", 11);
+
+        hs.add(new Student("小王" , 11));
+        System.out.println("s3 = " + s3.hashCode());
+
+        hs.add(new Student("小关" , 33));
+
+        Student[] students = hs.toArray(new Student[]{});
+
+        for(Student s : students){
+            System.out.println(s);
+        }
+
+    }
+}
+
+class Student {
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age &&
+                Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
